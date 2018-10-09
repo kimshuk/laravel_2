785a66d206e9b6be73c01404bbc3e7951b7c5bf0
developer <jeesoo2002@gmail.com>
2018-10-09 15:11:36 -0700

    complete eloquent

--- app/Task.php
+++ app/Task.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    public function isComplete() {
-        return false;
+    public function scopeIncomplete($query) {
+        return $query->where('completed', 0);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,15 +11,17 @@
 |
 */
 
+use App\Task;
+
 Route::get('/tasks', function () {
-    $tasks = App\Task::all();
+    $tasks = Task::all();
 
     return view('tasks.index', compact('tasks'));
 });
 
 Route::get('/tasks/{task}', function ($id) {
 
-    $task = DB::table('tasks')->find($id);
+    $task = Task::find($id);
 
     return view('tasks.show', compact('task'));
 });

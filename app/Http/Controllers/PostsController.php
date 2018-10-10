9963041e4b7302ac27f98045ffdb5e7d16c2774f
developer <jeesoo2002@gmail.com>
2018-10-10 16:19:07 -0700

    finishing form request

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -25,19 +25,8 @@ class PostsController extends Controller
 
     public function store()
     {
-
-//        $post = new Post;
-//
-//        $post->title = request('title');
-//
-//        $post->body = request('body');
-//
-//        $post->save();
-
-        Post::create([
-           'title' =>  request('title'),
-            'body' => request('body')
-        ]);
+        
+        Post::create(request(['title', 'body']));
 
         return redirect('/');
     }

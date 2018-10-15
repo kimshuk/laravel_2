7154a28cd79e334ebc49e21f8f31d1d7c925b25b
developer <jeesoo2002@gmail.com>
2018-10-15 15:50:55 -0700

    send email finished

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\Welcome;
 use App\User;
 
 class RegistrationController extends Controller
@@ -33,6 +34,8 @@ class RegistrationController extends Controller
 
         // Sign them in.
         auth()->login($user);
+
+        \Mail::to($user)->send(new Welcome($user));
 
         // Redirect to the home page.
 

76a8693ea0d971a1825c84c794769f33b595ed99
developer <jeesoo2002@gmail.com>
2018-10-15 15:06:25 -0700

    finishing service provider 2

--- routes/web.php
+++ routes/web.php
@@ -11,16 +11,12 @@
 |
 */
 
-App::bind('App\Billing\Stripe', function () {
-    return new \App\Billing\Stripe(config('services.stripe.secret'));
-
-});
 
 $stripe = resolve('App\Billing\Stripe');
 
 
 
-dd($stripe);
+//dd($stripe);
 
 Route::get('/', 'PostsController@index')->name('home');
 
